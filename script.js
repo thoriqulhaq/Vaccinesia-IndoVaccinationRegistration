8d1b7a6089e717c3152fc18fd59e0ccc6b6f16f1
Thoriqulhaq Jibril <thoriqulhaqjibril1@gmail.com>
2021-06-20 16:52:44 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var acc = document.getElementsByClassName("accordion1");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active1");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 8,
+    });
+  }
